64b91855fe7c7c33a9636ebf7579179ad561daf0
pouletgrill <xavierb.2006@hotmail.com>
2015-11-01 18:39:19 -0500

    qwertyuiop

---
+++ Sondage/test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PouletGrill
+ * Date: 2015-10-31
+ * Time: 16:42
+ */
+echo "Hello World";
+?>
---
+++ test2/gloup.php
@@ -0,0 +1,14 @@
+
+
+<html>
+<body>
+Sondage
+
+<?php $longeur = "100px";
+$pourcentage = "57%";
+echo"<div style='background-color: aqua; width: $longeur' >$pourcentage</div>";
+?>
+
+
+</body>
+</html>
