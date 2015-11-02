2fe8acad33a7fc25db251af777f0ff407e278cf1
PouletGrill <xavierb.2006@hotmail.com>
2015-11-02 12:18:26 -0500

    qwretyu

--- test2/vote_result.php
+++ test2/vote_result.php
@@ -8,21 +8,27 @@ $nombreParticipent = 0;
         <h1>Votre animal prefere</h1>
         <h2>Resultats obtenus pour <?php echo $nombreParticipent; ?> </h2>
 
-        <table border="1" >
+        <table border="0" >
             <tr>
                 <td>Chien</td>
                 <td>1</td>
-                <td>25%</td>
+                <td>
+                    <div style="background-color: aquamarine; width: 25px">25%</div>
+                </td>
             </tr>
             <tr>
                 <td>Chat</td>
                 <td>1</td>
-                <td>25%</td>
+                <td>
+                    <div style="background-color: chartreuse; width: 80px">80%</div>
+                </td>
             </tr>
             <tr>
                 <td>Oiseau</td>
                 <td>0</td>
-                <td>0%</td>
+                <td>
+                    <div style="background-color: blueviolet; width: 100px">100%</div>
+                </td>
             </tr>
         </table>
     </body>
