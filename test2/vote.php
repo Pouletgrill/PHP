d50980fe6cc467b522e6bd7f8587d9d520fab849
PouletGrill <xavierb.2006@hotmail.com>
2015-11-02 11:29:36 -0500

    the show must go on

---
+++ test2/vote.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 201354694
+ * Date: 2015-11-02
+ * Time: 11:28
+ */
---
+++ test2/vote_result.php
@@ -0,0 +1,29 @@
+<?php $longeur = "100px";
+$pourcentage = "57%";
+$nombreParticipent = 0;
+?>
+
+<html>
+    <body>
+        <h1>Votre animal prefere</h1>
+        <h2>Resultats obtenus pour <?php echo $nombreParticipent; ?> </h2>
+
+        <table border="1" >
+            <tr>
+                <td>Chien</td>
+                <td>1</td>
+                <td>25%</td>
+            </tr>
+            <tr>
+                <td>Chat</td>
+                <td>1</td>
+                <td>25%</td>
+            </tr>
+            <tr>
+                <td>Oiseau</td>
+                <td>0</td>
+                <td>0%</td>
+            </tr>
+        </table>
+    </body>
+</html>
